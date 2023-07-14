476d60ccad62fa3aece609b9918fb4f4fe588e56
Daniel Ozoani <57331891+OzunaModelo123@users.noreply.github.com>
2023-07-13 23:17:14 -0400

    Implemented binding and set up view and even listeners

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: ArrayAdapter<String>
+
+    /**
+     * Called when the activity is starting. Sets up the views and event listeners.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
