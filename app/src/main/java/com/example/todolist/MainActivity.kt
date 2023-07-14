ec185a3bb70c08c324ed739d127f2c4cfed0c635
Daniel Ozoani <57331891+OzunaModelo123@users.noreply.github.com>
2023-07-13 23:24:38 -0400

    Added object ArrayAdapter for the ListView, and used choiceMode to allow multiple selection in ListView

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.todolist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.example.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +17,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        // Create an empty list to store the to-do items
+        val items = mutableListOf<String>()
+
+        // Create an ArrayAdapter and set it as the adapter for the ListView
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, items)
+        binding.listView.adapter = adapter
+
+        // Set the choice mode of the ListView to allow multiple item selection
+        binding.listView.choiceMode = ListView.CHOICE_MODE_MULTIPLE
     }
 }
